a15e3b353ab21e4d50d900a865262dc9d1cb151c
Garrett Kelly <garrett@malechus.com>
2026-08-28 10:06:54 -0400

    adds build script

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+cd /srv/marvin/repo/marvin2
+git add .
+git stash
+git fetch
+git pull
+
+cd discord
+sudo dotnet build ./discord.csproj -c Release
+
+sudo cp -u ./bin/Release/net8.0/* /srv/marvin/prodsrv/
+
+sudo systemctl restart marvin-discord.service
